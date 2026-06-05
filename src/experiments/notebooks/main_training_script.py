92e92eac3307d6a636b8cd9730e498c12493bc7c
itonkdong <kostadinoskiviktor@yahoo.com>
2026-06-05 19:16:23 +0200

    fixed repo structure

--- src/experiments/notebooks/main_training_script.py
+++ src/experiments/notebooks/main_training_script.py
@@ -8,8 +8,8 @@ from pprint import pprint
 
 def main():
     exp_config = ExperimentConfig.get_from_yaml()
-    task = VmasTask.JOINT_PASSAGE_SIZE.get_from_yaml()
-    alg = MappoConfig.get_from_yaml()
+    task = VmasTask.BALANCE.get_from_yaml()
+    alg = MasacConfig.get_from_yaml()
     model = MlpConfig.get_from_yaml()
     critic_model = MlpConfig.get_from_yaml()
 
@@ -17,20 +17,26 @@ def main():
     # vars(test_env.scenario) - How to get these params
     # ==================================================
 
-    task.config["max_steps"] = 400
+    # task.config["max_steps"] = 400
+    # task.config["package_mass"] = 5
 
-    task.config["collision_reward"] = -0.015
-    task.config["use_vel_controller"] = True
-    task.config["max_speed_1"] = 0.25
-    task.config["observe_joint_angle"] = True
-    task.config["pos_shaping_factor"] = 3
-    task.config["rot_shaping_factor"] = 1.03
+
+    # task.config["collision_reward"] = -0.015
+    # task.config["use_vel_controller"] = True
+    # task.config["max_speed_1"] = 0.25
+    # task.config["observe_joint_angle"] = True
+    # task.config["pos_shaping_factor"] = 3
+    # task.config["rot_shaping_factor"] = 1.03
     # task.config
     # task.config["energy_reward_coeff"] = -0.01
     # task.config["use_vel_controller"] = True
     # task.config["max_speed_1"] = 0.7
     # task.config["observe_joint_angle"] = True
 
+    # alg.entropy_coef = 0.05
+    # exp_config.lr = 0.00002
+    # alg.critic_coef = 1.5
+    # model.num_cells = [256, 256, 128]
     # ==================================================
 
 
